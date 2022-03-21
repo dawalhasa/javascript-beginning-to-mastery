b0edd02646d46f0fa39c518e297b6d92c785de91
Dawa Lhasa <dawalhasa@gmail.com>
2022-03-21 16:39:19 +0530

    Enter

--- part2/how-javascript-works/hjw_10.js
+++ part2/how-javascript-works/hjw_10.js
@@ -13,11 +13,24 @@
 // console.log(ans2);
 
 
-function myFunction(power){
-    return function(number){
-        return number ** power
-    }
-}
+// function myFunction(power){
+//     return function(number){
+//         return number ** power
+//     }
+// }
+// const square = myFunction(2);
+// const ans = square(3);
+// console.log(ans);
+
+
+// const cube = myFunction(3);
+// const ans2 = cube(3);
+// console.log(ans2);
+// You can also make this shorter as example below;
+
+const myFunction = power => number => number ** power
+
+
 const square = myFunction(2);
 const ans = square(3);
 console.log(ans);
